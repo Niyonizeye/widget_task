df600e992494c1a65386fac502e5be97e68084a6
Niyonizeye <niyoeanpaul@gmail.com>
2022-02-23 14:11:44 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(()=>{
+    $("#my-widget1").click(()=>{
+      $(".textchanged").html("Hello world! Dialog boxes are one of the nice ways of presenting information on an HTML page.Dialog boxes are one of the nice ways of presenting information on an HTML page.");
+    });
+    $("#my-widget2").click(()=>{
+      $(".textchanged").html("<b>Hello world! Dialog boxes are one of the nice ways of presenting information on an HTML page.Dialog boxes are one of the nice ways of presenting information on an HTML page.</b>");
+    });
+    $("#my-widget3").click(()=>{
+      $(".textchanged").html("The text of three! Dialog boxes are one of the nice ways of Dialog boxes are one of the nice ways of presenting information on an HTML page.presenting information on an HTML page.");
+    });
+    $("#my-widget4").click(()=>{
+        $(".textchanged").html("Computer Quiz for SSC STEND Exams Computer Proficiency Test Sample Papers, Syllabus, Questions and Answers for various interviews, competitive exams and ...");
+      });
+      $("#my-widget5").click(()=>{
+        $(".textchanged").html("Computer Science Engineering (CSE), Information Technology (IT) Quiz, MCQS, Multiple Choice, Model Question Papers, Pdf, Free Download, Objective Type, Gk, ...");
+      });
+      $("#my-widget6").click(()=>{
+        $(".textchanged").html("The text of three! Dialog boxes are one of the nice ways of Dialog boxes are one of the nice ways of presenting information on an HTML page.presenting information on an HTML page.");
+      });
+  });
